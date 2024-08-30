9b62f4d6026abed5f7f66c5f693be04429c6971a
rahulbera9386 <rahulberastd2002@gmail.com>
2024-08-31 04:53:17 +0530

    Add Redux tookit with persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './Redux/Store.ts'
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore'
 
+let persistor = persistStore(store)
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+
+
 )
